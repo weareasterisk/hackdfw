207d8a806ba445f92cc2d9aa1a1fbc92ecdac1c0
Muhammad Khatri <muhammad@weareasterisk.com>
2019-06-16 23:21:32 -0500

    Added react-snap

--- src/index.js
+++ src/index.js
@@ -3,8 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import './styles/_aggregator.scss';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = document.getElementById('root');
+
+// If the DOM has child nodes, hydrate into an html string.
+// React-Snap uses this to pre-render pages instead of using JS
+// to hydrate the pages in the browser.
+root.hasChildNodes() ?
+  ReactDOM.hydrate(<App />, root) :
+  ReactDOM.render(<App/>, root);
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
