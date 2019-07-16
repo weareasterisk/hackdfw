ebd81ed132517c225c3c62949c8b64749fd9546d
Muhammad Khatri <muhammad@weareasterisk.com>
2019-07-15 22:26:52 -0500

    added faq data and re-ran yarn

---
+++ src/contentdata/faqdata.js
@@ -0,0 +1,13 @@
+const data = [
+  { question: "What is going oon", text: "this is going on roight he67890-09876re let me give you an explanation" },
+  { question: "what is going oon", text: "this is goiasdng on roight here let me give you an explanation" },
+  { question: "what is going oon", text: "this is going on asdasdasdasdasdroight here let me give you an explanation" },
+  { question: "what is going oon", text: "this asd1234is going on roight here let me give you an explanation" },
+  { question: "what is going oon", text: "this is going on roighasdsadasdaasdAa2222t here let me give you an explanation" },
+  { question: "what is going oon", text: "thiasds is going on roight here let me give you an explanation" },
+  { question: "what is going oon", text: "this is going on roight here let measd give you an explanation" },
+  { question: "what is going oon", text: "this is going on roighasdt here let me give you an explanation" }
+];
+
+
+export default data;
