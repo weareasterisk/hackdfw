359be0388b943e0a55a19a6a1794ec003d0950ee
Muhammad Khatri <muhammad@weareasterisk.com>
2019-07-15 22:24:46 -0500

    Added desktop version of faq section

---
+++ src/components/homepage/faq.js
@@ -0,0 +1,54 @@
+import React, {Component} from 'react';
+import Col from "reactstrap/es/Col";
+import ReactHtmlParser from 'react-html-parser';
+import Container from "reactstrap/es/Container";
+import {Row} from "reactstrap";
+
+export default class FAQ extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: props.data,
+      question: '',
+      text: ''
+    }
+  }
+
+  componentDidMount() {
+    this.changeFAQ(0);
+  }
+
+  changeFAQ = index => {
+    const { data } = this.state;
+    const {question, text} = data[index];
+    this.setState({question, text});
+  };
+
+  render() {
+    const { data, question, text} = this.state;
+    return (
+      <React.Fragment>
+        <Container>
+          <h1 className="faq-title">FAQ</h1>
+          <Row>
+            <Col md={4}>
+              {
+                data.map((entry, i) =>
+                  <p className="faq-question"
+                     onClick={() => this.changeFAQ(i)}
+                  >
+                    {entry.question}
+                  </p>
+                )
+              }
+            </Col>
+            <Col md={7}>
+              <h4 className="font-weight-medium"> {question} </h4>
+              {ReactHtmlParser(text)}
+            </Col>
+          </Row>
+        </Container>
+      </React.Fragment>
+    )
+  }
+}
--- src/components/pages/homepage.js
+++ src/components/pages/homepage.js
@@ -3,19 +3,34 @@ import SectionSpacer from "../sectionspacer"
 import Start from "../homepage/start"
 import About from "../homepage/about"
 import Schedule from "../homepage/schedule"
+import FAQ from "../homepage/faq"
 import ScheduleData from "../../contentdata/scheduledata"
+import FAQData from "../../contentdata/faqdata"
 
 export default class HomePage extends Component {
   render() {
     return (
       <React.Fragment>
         <Start/>
+
         <SectionSpacer/>
+
         <About/>
+
         <SectionSpacer/>
+
         <Schedule
           data={ScheduleData}
         />
+
+        <SectionSpacer/>
+
+        <FAQ
+          data={FAQData}
+        />
+
+        <SectionSpacer/>
+
       </React.Fragment>
     )
   }
